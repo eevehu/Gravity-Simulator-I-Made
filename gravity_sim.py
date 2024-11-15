f512c54aa53517dad1a5d4cfbcc2ddd11e300efd
eevehu <143351687+eevehu@users.noreply.github.com>
2024-11-15 20:17:59 +0000

    Add files via upload
    HaHa

---
+++ gravity_sim.py
@@ -0,0 +1,42 @@
+from decimal import Decimal, getcontext
+class gravitation():
+    # Setting the precision of Decimal up to 25 digits
+    getcontext().prec = 25
+    
+    def celestial_calculations(mAs1, mAs2, r, s):
+        g = Decimal("0.00000000006673")
+        m1 = gravitation.celestial_power(mAs1[0], mAs1[1])
+        m2 = gravitation.celestial_power(mAs2[0], mAs2[1])
+        # This code keeps the distance value within 100
+        divider = Decimal((100 / r))
+        # Settings the velocity for the acceleration calculations
+        velocity = Decimal(0)
+        while r > 0:
+            # Calculating the force in newtons
+            f = Decimal((g * m1 * m2) / (r * r))
+            
+            # This is the acceleration for the second object
+            a = Decimal((f/m2) / 10)
+            
+            
+            # Here I'm updating the velocity and distance over the simulated time
+            velocity += a * s
+            r -= velocity * s
+            if r < 0:
+                r = 0
+            print(f"Velocity: {a} R Distance: {r * divider}")
+        main()
+    
+    def celestial_power(n, s):
+        return Decimal(n) ** Decimal(s)
+        
+def main():
+    mass1 = int(input("Gravity Simulater: SUPERSCRIPT 1: "))
+    mass2 = int(input("Gravity Simulater: SUPERSCRIPT 2: "))
+    r = int(input("Gravity Simulater: DISTANCE: "))
+    simSpeed = int(input("Gravity Simulater: SPEED Multiplier for Simulation: "))
+    gravitation.celestial_calculations([10, mass1], [10, mass2], r, simSpeed)
+    
+    
+if __name__ == "__main__":
+    main()
